353dd53e9050e847d1b83af75d8f52a243403862
UjjwalUpadhyay14 <142168540+UjjwalUpadhyay14@users.noreply.github.com>
2024-10-15 11:37:21 +0530

    Add files via upload
    15 oct string

---
+++ 15-oct.js
@@ -0,0 +1,57 @@
+// Primitive DataTypes
+
+// String
+// Number
+// boolean
+// undefined
+// null
+// symbol
+// BigInt
+
+// Non-Primitive datatypes
+// Array
+// Object
+// function
+
+// String => String is a Primitive datatype we can intialize in variable anything but within "" or ''
+
+// var uj = "Ujjwal"
+
+// console.log(uj)
+
+// console.log(typeof(uj))
+
+// const a = "ketu"
+
+// a= "iko"
+
+// {
+//  var a = 45
+// }
+
+// console.log(a)
+
+// const uj = "ujio"
+// console.log(uj)
+
+var str = "Akshey ";
+
+console.log(str.length);
+
+console.log(str.toUpperCase());
+
+console.log(str.toLowerCase());
+
+console.log(str.charAt(5));
+
+console.log(str.substring(4, 8));
+
+console.log(str.slice(4, -2));
+
+console.log(str);
+
+console.log(str.trim());
+
+console.log(str.replace("kumar", "AKshey"));
+
+console.log(str.split(","));
