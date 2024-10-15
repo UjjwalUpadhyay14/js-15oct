6519cd3daa630f95e178bb3510d7ddd42436e9d3
UjjwalUpadhyay14 <142168540+UjjwalUpadhyay14@users.noreply.github.com>
2024-10-15 11:54:33 +0530

    Update 15-oct.js
    new changes

--- 15-oct.js
+++ 15-oct.js
@@ -55,3 +55,21 @@ console.log(str.trim());
 console.log(str.replace("kumar", "AKshey"));
 
 console.log(str.split(","));
+
+
+// var new_date = new Date()
+
+// console.log(new_date.toTimeString())
+
+
+// var date = new Date('2024-12-14')
+
+// console.log(date)
+
+
+// console.log(Date.now()/1000)
+
+
+
+
+// Math Function 
