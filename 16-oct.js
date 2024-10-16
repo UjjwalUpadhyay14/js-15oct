25f02873bd74024ed7c9133336d5ad2e1151944a
UjjwalUpadhyay14 <142168540+UjjwalUpadhyay14@users.noreply.github.com>
2024-10-16 11:58:49 +0530

    Add files via upload
    update 16-oct

---
+++ 16-oct.js
@@ -0,0 +1,176 @@
+// var a = -89
+
+// console.log(Math.abs(a))
+
+// //    1-4 => cuur 5-9 => nex value
+
+// console.log(Math.round(num))
+
+// console.log(Math.ceil(num))
+
+// var num = 5.9
+
+// console.log(Math.floor(num))
+
+// var a = 9
+
+// console.log(Math.sqrt(a))
+
+// var a = 5;
+// var b = 6;
+// var c = 9;
+// var d = 3;
+
+
+// // console.log(Math.max(a,b,c,d))
+
+// console.log(Math.min(a,b,c,d))
+
+
+// console.log(Math.floor(Math.random()*10 + 1))
+
+// var arr = [1,2,3,4,5]
+
+// console.log(arr[3])
+
+// arr[2] = 9;
+
+// console.log(arr)
+
+
+// arr.push(7);
+
+
+// console.log(arr)
+
+// arr.unshift(0)
+
+// console.log(arr)
+
+// arr.pop()
+
+// console.log(arr)
+
+// arr.shift()
+
+// console.log(arr)
+
+
+// console.log(arr.length) 
+
+
+
+
+// console.log(arr.join(' '))
+
+
+
+
+// var a = 4;
+
+// console.log(a)
+
+
+
+
+// var new_team = arr.slice(0,3)
+
+// console.log(new_team)
+
+
+// console.log(arr)
+
+
+// var arr = ["Rohit","virat","Bumrah","Hardik","KL"]
+
+
+// var new_team = arr.splice(0,3)
+
+// console.log(new_team)
+
+
+// console.log(arr)
+// var array = [1,2,[3,4,[5,6,[7,8,[9,10,[11,12,[14,15,[16,17,[18,19]]]]]]]]]
+
+// console.log(array[2][2][2][2][2][2][2][2][1])
+
+
+
+// var new_Arr = array.flat(Infinity)
+
+
+// console.log(new_Arr[18])
+
+
+
+// var Batsman = ["Rohit","Virat","KL Rahul","Jaiswal","Gill"]
+// var Bowler = ["Bumrah","Bhuvneshwar","Shami","Arshdeep"]
+
+
+// var Team = Batsman.concat(Bowler)
+// console.log(Team)
+
+// var Team = [...Batsman,...Bowler]
+// console.log(Team)
+
+
+
+
+
+
+// Object := Object is also non premitive datatype in js , but it stores value as key value pair, it also allows to store any data type variables but not duplicate value
+
+
+// var user = {
+//     name :"Soliden",
+//     age:45
+// }
+
+// console.log(user)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
