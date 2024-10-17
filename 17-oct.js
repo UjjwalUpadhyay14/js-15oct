796bce8248d4e24b1904cbd72dc7158ee847b445
UjjwalUpadhyay14 <142168540+UjjwalUpadhyay14@users.noreply.github.com>
2024-10-17 11:48:44 +0530

    Add files via upload
    new update

---
+++ 17-oct.js
@@ -0,0 +1,225 @@
+// var obj = {
+//     name:"Vk",
+//     email:"vk@gama9iil.com",
+//     age:45,
+//     mobile :1653161661,
+//     active:true
+// }
+
+
+// console.log(obj["name"])
+
+
+
+// console.log(obj.mobile)
+
+
+// obj.name = "vicy"
+
+
+// console.log(obj)
+
+// Object.freeze(obj)
+
+// obj.name = "Aman"
+
+// console.log(obj)
+
+
+// const user = {
+//     name:"aman",
+//     age:45,
+//     email:"aman@gmail.com",
+//     address:{
+//         street:"abc",
+//         city:"pune",
+//         state:"maharashtra"
+//     }
+// }
+
+
+// console.log(user.address.state)   
+
+
+
+// const user = {
+//     first_name:"Salman",
+//     last_name:" Khan",
+//     full_name:function(){
+//         console.log(this.first_name + this.last_name)
+//     }
+
+// }
+
+
+// console.log(user.full_name())
+
+
+
+// backticks 
+
+
+// var first_name = "Ujjwal"
+
+// var last_name = "Upadhyay"
+
+// console.log(`${first_name} ${last_name}`)
+
+
+// var cart = [
+//     {
+//         id:1,
+//         product_name:"pencile",
+//         product_price:"455$"
+//     },
+//     {
+//         id:2,
+//         product_name:"cutter",
+//         product_price:"45$"
+//     }
+
+// ]
+
+
+
+// console.log(cart[1].product_price)
+
+
+
+
+// var uttar_pradesh = {
+//     city_1:"Kanpur",
+//     city_2:"Lucknow",
+//     city_3:"Agra",
+//     city_4:"Varanshi"
+// }
+
+// var maharashtra ={
+//     city_5:"Mumbai",
+//     city_6:"Pune",
+//     city_7:"Aurangabaad",
+//     city_8:"Nagpur"
+// }
+
+
+// var india = {...uttar_pradesh,...maharashtra}
+
+// console.log(india)
+
+// function printf(some){
+//     return some;
+// }
+
+
+// console.log(printf("Ujjwal"))
+
+
+// function print(){
+//     console.log("Hello world");
+// }
+
+
+// print()
+
+
+// function add(a,b){
+//     return a+b;
+// }
+
+// console.log(add(45,45))
+
+
+// var add = function (a,b){
+//     console.log(a+b)
+// }
+
+
+// add(4,5)
+
+
+// var multiply = (num1,num2)=> console.log(num1*num2)
+
+// multiply(5,9)
+
+
+// iife 
+
+
+// (function print(){console.log("Hello")}) ();
+
+
+// const calc =(a,b)=>{
+//     console.log(a+b)
+//     console.log(a*b)
+//     console.log(a-b)
+//     console.log(a/b)
+//     console.log(a%b)
+
+// }
+
+
+// calc(6,5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
