47172f393dc47160740d0dba4325fb171d1a731d
UjjwalUpadhyay14 <142168540+UjjwalUpadhyay14@users.noreply.github.com>
2024-10-19 11:30:42 +0530

    code upadtae
    19-oct

---
+++ 18-oct.js
@@ -0,0 +1,177 @@
+//
+
+// var user_id = "ujjwal"
+// var password = "uj@140"
+
+// if(user_id=="ujjwal"){
+
+//     if(password=="uj@1403"){
+//         console.log("Login Successfull");
+//     }
+
+//     else{
+//         console.log("Invalid Password");
+//     }
+// }
+
+// var pin=1234;
+
+// switch (pin) {
+//     case 1403: console.log("Helloo code_uj")
+//         break;
+
+//     case 2303:console.log("Hello pr")
+//         break;
+
+//     case 1234: console.log("hello random user")
+//         break;
+
+//     default:
+
+//         console.log("invalid")
+//         break;
+// }
+
+// iteration ==>
+
+var cart = [
+  {
+    product_name: "Apple",
+    price: 200,
+  },
+  {
+    product_name: "Orange",
+    price: 500,
+  },
+  {
+    product_name: "Banana",
+    price: 60,
+  },
+];
+
+
+// for(let array=0; array<cart.length; array++ ){
+//     console.log(cart[array].price)
+// }
+
+// let i =1
+
+// while(i<=10){
+
+//     if(i==5){
+//         i++;
+//         continue;
+//     }
+//     console.log(i)
+//     i++;
+
+// }
+
+// let i = 5
+
+// do{
+//   console.log(i)
+// }while(i==6)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   simple loops
+
+// for
+// while
+// do while
+
+// high order array loops
+
+// for of
+// for in
+// for each
+// map
+// filter
+// reduce
+
+
+
+// for (const num of arr) {
+  //     console.log(num)
+// } 
+
+var user = {
+  name: 'John',
+  age: 30
+}
+
+
+// for (const key in user) {
+  //       console.log(user[key])
+  //   }
+  
+  
+  
+  
+  // for (const [index,item] in arr) {
+    
+  //   console.log(`${index} ==> ${arr[index]}`)
+  
+  // }
+  
+  
+  
+  var arr = [1,2,3,4,5]
+
+
+  // arr.forEach((item,index,arr)=>{
+  //       console.log(item,index,arr)
+  // })
+
+
+
+  // var new_arr =arr.map((item)=>item*100)
+
+
+  // console.log(new_arr)
+
+
+
+
+  // var new_arr = arr.filter((item)=>item>3)
+
+  // console.log(new_arr)
+
+
+  var total = arr.reduce((acc,crr)=>acc+crr)
+
+  console.log(total)
+
+
+
+
+
+
+
+
+
+
+
+
